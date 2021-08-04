09929c9eae6a1e3d5e40d05103720928e8918989
Toki <55911995+tokio-k@users.noreply.github.com>
2021-08-04 18:51:53 +0900

    update/RESTfull表現の変更、リンク追加

--- restfull/src/main/java/tutorials/restfull/EmployeeController.java
+++ restfull/src/main/java/tutorials/restfull/EmployeeController.java
@@ -1,22 +1,35 @@
 package tutorials.restfull;
 
 import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("employees")
 public class EmployeeController {
     private final EmployeeRepository repository;
 
-    EmployeeController(EmployeeRepository repository) {
+    private final EmployeeModelAssembler assembler;
+
+    EmployeeController(EmployeeRepository repository, EmployeeModelAssembler assembler) {
         this.repository = repository;
+        this.assembler = assembler;
     }
 
     @GetMapping("")
-    List<Employee> searchAllEmployees(){
-        return repository.findAll();
+    CollectionModel<EntityModel<Employee>> searchAllEmployees(){
+        List<EntityModel<Employee>> employees = repository.findAll().stream()
+                .map(assembler::toModel)
+                .collect(Collectors.toList());
+        return CollectionModel.of(employees, linkTo(methodOn(EmployeeController.class).searchAllEmployees()).withSelfRel());
     }
 
     @PostMapping("")
@@ -25,11 +38,9 @@ public class EmployeeController {
     }
 
     @GetMapping("/id/{id}")
-    Employee searchEmployee(@PathVariable Long id){
-        Employee  employee;
-
-            employee = repository.findById(id).orElseThrow(() -> new EmployeeNotFoundException(id));
-        return employee;
+    EntityModel<Employee> searchEmployee(@PathVariable Long id){
+        Employee  employee = repository.findById(id).orElseThrow(() -> new EmployeeNotFoundException(id));
+        return assembler.toModel(employee);
     }
 
     @PutMapping("/id/{id}")
