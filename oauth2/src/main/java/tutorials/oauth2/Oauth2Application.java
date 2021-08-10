9a8a0ab8a17b0d5f261bb11603d3e4fbfebf15ad
Toki <55911995+tokio-k@users.noreply.github.com>
2021-08-11 08:21:28 +0900

    update oauth

--- oauth2/src/main/java/tutorials/oauth2/Oauth2Application.java
+++ oauth2/src/main/java/tutorials/oauth2/Oauth2Application.java
@@ -2,12 +2,43 @@ package tutorials.oauth2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
 
 @SpringBootApplication
-public class Oauth2Application {
+@RestController
+public class Oauth2Application extends WebSecurityConfigurerAdapter {
+
+	@GetMapping("/user")
+	public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+		return Collections.singletonMap("name", principal.getAttribute("name"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Oauth2Application.class, args);
 	}
 
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// @formatter:off
+		http
+				.authorizeRequests(a -> a
+						.antMatchers("/", "/error", "/webjars/**").permitAll()
+						.anyRequest().authenticated()
+				)
+				.exceptionHandling(e -> e
+						.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+				)
+				.oauth2Login();
+		// @formatter:on
+	}
 }
